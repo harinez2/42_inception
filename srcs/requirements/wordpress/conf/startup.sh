7b63eeb5214c8a0d2756d444a6a72604d05d3c49
harinez2 <yonishi@student.42tokyo.jp>
2023-03-31 02:56:18 +0900

    added editor account on wp, added original account id for wp admin user

--- srcs/requirements/wordpress/conf/startup.sh
+++ srcs/requirements/wordpress/conf/startup.sh
@@ -32,10 +32,22 @@ echo "wp core install-------------------" >>$log_file
 wp core install --allow-root \
 	--url=https://${SITE_DOMAIN_NAME} \
 	--title="${WP_SITE_TITLE}" \
-	--admin_user=${MYSQL_USER_NAME} \
-	--admin_password=${MYSQL_USER_PASSWD} \
+	--admin_user=${WP_ADMIN_NAME} \
+	--admin_password=${WP_ADMIN_PASSWD} \
 	--admin_email=${SITE_EMAIL} \
 	>>$log_file
+
+# add editor user to wp
+echo "wp user create-------------------" >>$log_file
+wp user create \
+	${WP_EDITOR_NAME} \
+	"dummy@example.com" \
+	--role=editor \
+	--user_pass=${WP_EDITOR_PASSWD} \
+	--allow-root \
+	>>$log_file
+
+echo -e "\n" >>$log_file
 
 ################################################################
 # php
